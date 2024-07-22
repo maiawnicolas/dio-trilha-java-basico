2ee35ceb5d424c68d91602c068f9a2ff5469d9be
Nicolas Maia <nicolaswmaia@gmail.com>
2024-07-22 14:28:31 -0300

    att

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite a agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        Double saldo = 237.48;
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ "
+         + saldo + " já está disponível para saque.");
+
+
     }
 }
